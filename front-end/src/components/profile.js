db16e5793cb0ce81f098ce0de30cf5bf10f3ea9d
Miguel Peralta <eudysperalta723@gmail.com>
2020-05-04 23:40:30 -0700

    profile component

---
+++ front-end/src/components/profile.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const NavBar = () => {
+    return (
+        <div>
+            <a href="#">homme</a>
+            <a href="#">projects</a>
+            <a href="#">contact</a>
+        </div>
+    )
+}
+
+
+export default NavBar;
