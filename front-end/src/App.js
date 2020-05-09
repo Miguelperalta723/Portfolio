23f042a9e9a82f59a6a6bd7965628657edc3b991
Miguel Peralta <eudysperalta723@gmail.com>
2020-05-08 21:49:15 -0700

    basic contact form

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Switch , Route } from "react-router-dom";
 
 
 import "./App.scss"
@@ -7,11 +7,15 @@ import "./App.scss"
 //components
 import NavBar from "./components/navbar.js"
 import Footer from "./components/footer.js"
+import Contact from "./components/contact.js"
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+        <Switch>
+          <Route path="/contact" component={Contact}/>
+        </Switch>
       <Footer />
     </div>
   );
---
+++ front-end/src/components/contact.js
@@ -0,0 +1,41 @@
+import React, {useState} from "react";
+
+const Contact = () => {
+    const [ sender , setSender] = useState({name: "", email: "", message: ""})
+
+    return(
+        <div>
+            <form>
+                <label>
+                    Name
+                    <input 
+                        type="text"
+                        name="name"
+                        value={sender.name}
+                    />
+                </label>
+                <label>
+                    Email
+                    <input 
+                        type="text"
+                        name="email"
+                        value={sender.email}
+                    />
+                </label>
+                <label>
+                    Message
+                    <input 
+                        type="text"
+                        name="message"
+                        id="message-box"
+                        value={sender.message}
+                    />
+                </label>
+                <button>Send!</button>
+            </form>
+        </div>
+    )
+}
+
+
+export default Contact;
